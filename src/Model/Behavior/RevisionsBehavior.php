1ac657d82bdec1758b147b9e740a289ba29ab167
Chris Brescia <chris@cwbit.com>
2015-05-28 15:45:32 -0400

    updated to use patchEntity
    does a shallow clone of entity because for some reason `patchEntity($a,
    [ .. array .. ] )` will actually modify `$a` itself.. no good.
    
    fixes #3

--- src/Model/Behavior/RevisionsBehavior.php
+++ src/Model/Behavior/RevisionsBehavior.php
@@ -9,17 +9,30 @@ use Cake\ORM\TableRegistry;
 
 class RevisionsBehavior extends Behavior{
 	
-	// private $_before = null;
-
+	
+	/**
+	 * Use config to specify the Models you want to watch, and the fields you want to monitor
+	 * @var array
+	 */
 	public $_config = [
 		'Model' => [
-			# fields
+			'watch' => [ .. ],	# (whitelist) only watch these fields
+			'ignore' => [ .. ], # (blacklist) modifications to these fields will be ignored
 		],
 	];
 
+	/**
+	 * Save the before and after state of the modified entity to the Revisions table.
+	 * 
+	 * @todo  clones the object because patchEntity modified entity itself (bug?)
+	 * @param  Event  $event  [description]
+	 * @param  Entity $entity [description]
+	 * @return void
+	 */
 	public function afterSave( Event $event, Entity $entity){
+		
 		# rebuild the original entity
-		$before = $this->_table->newEntity($entity->extractOriginal($this->_table->schema()->columns()));
+		$before = $this->_table->patchEntity($before = clone $entity, $before->extractOriginal($this->_table->schema()->columns()));
 
 		# load the Revisions Model
 		$this->Revisions = TableRegistry::get('Revisions.Revisions');
