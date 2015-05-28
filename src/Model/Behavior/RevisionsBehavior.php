dad7e6e2dcc5888135eeef5b1e78ee569d0aea07
Chris Brescia <chris@cwbit.com>
2015-05-28 13:06:01 -0400

    rough-in afterSave

--- src/Model/Behavior/RevisionsBehavior.php
+++ src/Model/Behavior/RevisionsBehavior.php
@@ -2,9 +2,23 @@
 
 namespace Revisions\Model\Behavior;
 
+use Cake\Event\Event;
+use Cake\ORM\Entity;
 use Cake\ORM\Behavior;
 
 class RevisionsBehavior extends Behavior{
 	
-	
+	public $_config = [
+		'Model' => [
+			# fields
+		],
+	];
+
+	public function afterSave( Event $event, Entity $entity)
+	{
+
+		debug($entity);
+
+	}	
+
 }
