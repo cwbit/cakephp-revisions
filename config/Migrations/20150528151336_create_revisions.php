a59b097bb0c34552d292abc2f6630b4e51bd1291
Chris Brescia <chris@cwbit.com>
2015-05-28 11:15:35 -0400

    migrations for Revisions table

---
+++ config/Migrations/20150528151336_create_revisions.php
@@ -0,0 +1,48 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class CreateRevisions extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('revisions', ['id' => false, 'primary_key' => ['id']]);
+        $table->addColumn('id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('model', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('modelPrimaryKey', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('before', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('after', 'text', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
