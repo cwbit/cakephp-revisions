1ed710f314d5b42283cec86c374398c9286d0be7
Chris Brescia <chris@cwbit.com>
2015-05-28 13:26:12 -0400

    latest

--- src/Model/Behavior/RevisionsBehavior.php
+++ src/Model/Behavior/RevisionsBehavior.php
@@ -8,17 +8,23 @@ use Cake\ORM\Behavior;
 
 class RevisionsBehavior extends Behavior{
 	
+	// private $_before = null;
+
 	public $_config = [
 		'Model' => [
 			# fields
 		],
 	];
 
-	public function afterSave( Event $event, Entity $entity)
-	{
+	// public function beforeSave(Event $event, Entity $entity){
+	// 	$this->_before = $entity->jsonSerialize();
+	// }
 
+	public function afterSave( Event $event, Entity $entity){
+		$before = $this->_table->patchEntity($entity, $entity->extractOriginal($entity->_accessible));
+
+		debug($before);
 		debug($entity);
-
 	}	
 
 }
